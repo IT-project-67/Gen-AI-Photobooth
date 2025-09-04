a7237fd031c80383a5a7a06cb605713fa08a2051
yumchen3 <yumeng.chen5@student.unimelb.edu.au>
2025-09-04 17:55:08 +1000

    upload basic feature and edit nuxt.config.ts and add main.css.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,4 +32,19 @@ export default defineNuxtConfig({
       },
     },
   },
+
+  // css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: "Gen-AI Photobooth",
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Gen-AI PhotoBooth Web App' }
+      ],
+      link: [
+        {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+      ]
+    }
+  }
 });
