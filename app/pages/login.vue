21bc8cd8daed3dcb0aa33407b0825a086baf1583
Tong Wu <tww8@student.unimelb.edu.au>
2025-09-08 15:55:50 +1000

    feature: Implemented login page

---
+++ app/pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="login-page">
+        <AuthForm
+            title="Log in to Your Account"
+            buttonText="Log in"
+            socialLoginText="sign up"
+            :showConfirmPassword="false"
+        />
+    </div>
+</template>
+  
+<style>
+    html, body {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
+
+    .login-page {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 50vh;
+        overflow: hidden; /* prevent scrollbars */
+        box-sizing: border-box;
+        padding: 0 16px; /* horizontal padding only */
+    }
+</style>
