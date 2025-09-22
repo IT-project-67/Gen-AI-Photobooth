5901d269d1e97527f89506a5f41073c4419dd926
Zishen-019 <ZISHENL@student.unimelb.edu.au>
2025-09-22 14:22:15 +1000

    chore(config): fix the config try to make the vercel work

--- server/config.ts
+++ server/config.ts
@@ -1,5 +1,10 @@
-export const config = {
-  STORAGE_BUCKET: useRuntimeConfig().storageBucket ?? "PhotoBooth",
-} satisfies {
+type AppConfig = {
   STORAGE_BUCKET: string;
+};
+
+export const config = (): AppConfig => {
+  const rc = useRuntimeConfig();
+  return {
+    STORAGE_BUCKET: rc.storageBucket ?? "PhotoBooth",
+  };
 };
