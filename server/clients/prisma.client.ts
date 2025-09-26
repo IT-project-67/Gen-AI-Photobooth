a3621d73dd0f56231b7ac69c81c672266dab7f0f
Zishen-019 <ZISHENL@student.unimelb.edu.au>
2025-09-26 22:00:14 +1000

    fix(login): try to make more attempts of login to slove the unexpected error

--- server/clients/prisma.client.ts
+++ server/clients/prisma.client.ts
@@ -4,7 +4,15 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+  log: ["error"],
+});
+
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
