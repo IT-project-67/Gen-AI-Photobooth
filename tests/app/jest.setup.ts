4eef8cfde41ee4eabcc47f4cc86146ea681d6d10
CHENGHAO YANG <chenghaoy2@student.unimelb.edu.au>
2025-10-12 16:45:00 +1100

    test(frontend): fixed console issue

--- tests/app/jest.setup.ts
+++ tests/app/jest.setup.ts
@@ -1,6 +1,15 @@
 import { jest } from "@jest/globals";
 import { ref as vueRef, readonly as vueReadonly } from "vue";
 
+global.console = {
+  ...console,
+  log: jest.fn(),
+  debug: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+};
+
 const mockGetSession = jest.fn(() =>
   Promise.resolve({
     data: {
